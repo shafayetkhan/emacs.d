69d0da7a2176e8dcffc2eb820d0b08257b50c172
Shafayet Khan <shafi@layer3tv.com>
2015-05-08 14:57:47 -0400

    Change theme to tomorrow-night

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -12,7 +12,7 @@
 ;;------------------------------------------------------------------------------
 ;; Old-style color theming support (via color-theme.el)
 ;;------------------------------------------------------------------------------
-(defcustom window-system-color-theme 'color-theme-sanityinc-solarized-dark
+(defcustom window-system-color-theme 'color-theme-sanityinc-tomorrow-night
   "Color theme to use in window-system frames.
 If Emacs' native theme support is available, this setting is
 ignored: use `custom-enabled-themes' instead."
