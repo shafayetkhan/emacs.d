7e425f70036840ec50722eac49c066179f24257f
Shafayet Khan <shafi@layer3tv.com>
2015-06-03 23:52:42 -0400

    Remove custom.el from .gitignore

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (brin)))
+ '(custom-safe-themes
+   (quote
+    ("7bde52fdac7ac54d00f3d4c559f2f7aa899311655e7eb20ec5491f3b5c533fe8" "9f3a4edb56d094366afed2a9ba3311bbced0f32ca44a47a765d8ef4ce5b8e4ea" default)))
+ '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(diff-refine-added ((t (:inherit diff-refine-change :background "SpringGreen4"))))
+ '(diff-refine-removed ((t (:inherit diff-refine-change :background "firebrick4"))))
+ '(mode-line ((t (:background "#292B2D" :foreground "#3A85BD"))))
+ '(mode-line-buffer-id ((t (:foreground "SpringGreen3")))))
