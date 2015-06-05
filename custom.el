2d67303d473550d65df65af6cc4b35656a3712b8
Shafayet Khan <shafi@layer3tv.com>
2015-06-04 22:37:44 -0400

    Change order of loading themes

--- custom.el
+++ custom.el
@@ -3,11 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (brin)))
+ '(custom-enabled-themes (quote (brin base16-default-dark)))
  '(custom-safe-themes
    (quote
-    ("7bde52fdac7ac54d00f3d4c559f2f7aa899311655e7eb20ec5491f3b5c533fe8" "9f3a4edb56d094366afed2a9ba3311bbced0f32ca44a47a765d8ef4ce5b8e4ea" default)))
- '(session-use-package t nil (session)))
+    ("7bde52fdac7ac54d00f3d4c559f2f7aa899311655e7eb20ec5491f3b5c533fe8" "9f3a4edb56d094366afed2a9ba3311bbced0f32ca44a47a765d8ef4ce5b8e4ea" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
