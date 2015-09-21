35a5cdc6071641191d1e909d174cdd898e5ab6ab
Shafayet Khan <shafi@layer3tv.com>
2015-09-20 21:48:31 -0400

    Change solarized to sanityic-tomorrow and add powerline

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -7,7 +7,7 @@
 ;;------------------------------------------------------------------------------
 ;; Old-style color theming support (via color-theme.el)
 ;;------------------------------------------------------------------------------
-(defcustom window-system-color-theme 'color-theme-sanityinc-solarized-dark
+(defcustom window-system-color-theme 'color-theme-sanityinc-tomorrow-night
   "Color theme to use in window-system frames.
 If Emacs' native theme support is available, this setting is
 ignored: use `custom-enabled-themes' instead."
@@ -22,7 +22,7 @@ ignored: use `custom-enabled-themes' instead."
 (unless (boundp 'custom-enabled-themes)
   (defun color-theme-terminal ()
     (interactive)
-    (color-theme-sanityinc-solarized-dark))
+    (color-theme-sanityinc-tomorrow-night))
 
   (defun apply-best-color-theme-for-frame-type (frame)
     (with-selected-frame frame
@@ -45,7 +45,7 @@ ignored: use `custom-enabled-themes' instead."
 ;;------------------------------------------------------------------------------
 
 ;; If you don't customize it, this is the theme you get.
-(setq-default custom-enabled-themes '(sanityinc-solarized-light))
+(setq-default custom-enabled-themes '(sanityinc-tomorrow-night))
 
 ;; Ensure that themes will be applied even if they have not been customized
 (defun reapply-themes ()
@@ -64,12 +64,20 @@ ignored: use `custom-enabled-themes' instead."
 (defun light ()
   "Activate a light color theme."
   (interactive)
-  (color-theme-sanityinc-solarized-light))
+  (color-theme-sanityinc-solarized-light)
+  (powerline-reset))
 
 (defun dark ()
   "Activate a dark color theme."
   (interactive)
-  (color-theme-sanityinc-solarized-dark))
+  (color-theme-sanityinc-tomorrow-night)
+  (powerline-reset))
+
+;;------------------------------------------------------------------------------
+;; Activate powerline
+;;------------------------------------------------------------------------------
+(require-package 'powerline)
+(add-hook 'after-init-hook 'powerline-default-theme)
 
 
 (provide 'init-themes)
