248586f72bf048df8db2fe40eb72c374a68ba3b4
Shafayet Khan <shafi@layer3tv.com>
2015-11-15 14:08:59 -0500

    Deativate kefreq package because of instability

--- lisp/init-stats.el
+++ lisp/init-stats.el
@@ -1,7 +1,7 @@
 ;; <shafi> Track emacs commands frequency to better understand usage
-(require-package 'keyfreq)
-(keyfreq-mode 1)
-(keyfreq-autosave-mode 1)
+;; (require-package 'keyfreq)
+;; (keyfreq-mode 1)
+;; (keyfreq-autosave-mode 1)
 
 ;; Use keyfreq-show to see how many times you used a command
 
